8735894859aa33e5de99f33bcd01ca8dbf6bd47e
abhiamber191 <abhiamber191@gmail.com>
2022-10-25 11:42:03 +0530

    hash

--- kindmeal-clone-project/src/index.js
+++ kindmeal-clone-project/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import ContextProvider from './Context/AuthContext'
 // import corse from 'c'
 
@@ -11,11 +11,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
   <ContextProvider>
-  <BrowserRouter>
+  <HashRouter>
 
   
     <App />
-    </BrowserRouter>
+    </HashRouter>
     </ContextProvider>
   </React.StrictMode>
 );
